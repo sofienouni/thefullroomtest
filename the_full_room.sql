43853d7a39a915eb4ef968c30dc3a5cb22699bf9
sofienouni <110794072+sofienouni@users.noreply.github.com>
2023-01-21 17:29:15 +0100

    DB

---
+++ the_full_room.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 21 jan. 2023 à 16:28
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `the_full_room`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conges`
+--
+
+DROP TABLE IF EXISTS `conges`;
+CREATE TABLE IF NOT EXISTS `conges` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `comment` longtext COLLATE utf8mb4_unicode_ci,
+  `status_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6327DE3A6BF700BD` (`status_id`),
+  KEY `IDX_6327DE3AA76ED395` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `conges`
+--
+
+INSERT INTO `conges` (`id`, `start_date`, `end_date`, `comment`, `status_id`, `user_id`) VALUES
+(1, '2023-04-01', '2023-04-08', NULL, 2, 1),
+(2, '2023-10-30', '2023-11-02', NULL, 3, 1),
+(3, '2023-08-01', '2023-08-03', NULL, 3, 1),
+(4, '2023-12-10', '2023-12-13', NULL, 2, 1),
+(5, '2023-09-17', '2023-09-18', NULL, 2, 2),
+(6, '2023-04-27', '2023-05-03', NULL, 3, 2),
+(7, '2023-10-08', '2023-10-17', NULL, 3, 1),
+(8, '2023-10-12', '2023-10-15', NULL, 2, 1),
+(9, '2023-09-06', '2023-09-16', NULL, 3, 2),
+(10, '2023-08-15', '2023-08-24', NULL, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20230121132041', '2023-01-21 13:20:54', 76),
+('DoctrineMigrations\\Version20230121132236', '2023-01-21 13:22:40', 81),
+('DoctrineMigrations\\Version20230121132431', '2023-01-21 13:24:35', 82),
+('DoctrineMigrations\\Version20230121140944', '2023-01-21 14:11:09', 105);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+DROP TABLE IF EXISTS `messenger_messages`;
+CREATE TABLE IF NOT EXISTS `messenger_messages` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `status_conges`
+--
+
+DROP TABLE IF EXISTS `status_conges`;
+CREATE TABLE IF NOT EXISTS `status_conges` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `status_conges`
+--
+
+INSERT INTO `status_conges` (`id`, `status`) VALUES
+(1, 'en attente'),
+(2, 'approuvé'),
+(3, 'refusé');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `roles`, `password`) VALUES
+(1, 'sofien', '[\"ROLE_ADMIN\"]', '$2y$13$txH1tIMGsKMvGat0Ojcce.P3Y9RxExMZdeDret5ECuAWrcnnzPQxG'),
+(2, 'user1', '[\"ROLE_USER\"]', '$2y$13$ZNKD01jTJoQDfIXgwycJq.iFrhHDWjNOg1lryXCtfOCHDJrTDVjgO');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `conges`
+--
+ALTER TABLE `conges`
+  ADD CONSTRAINT `FK_6327DE3A6BF700BD` FOREIGN KEY (`status_id`) REFERENCES `status_conges` (`id`),
+  ADD CONSTRAINT `FK_6327DE3AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
